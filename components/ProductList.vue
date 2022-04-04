4a656556cc54330b2b498204ff61af42aca6461d
mwchoen <mwcheon@cudo.co.kr>
2022-04-05 08:39:18 +0900

    aysncData()를 사용해 서버사이드 렌더링으로 데이터 가져오기

---
+++ components/ProductList.vue
@@ -0,0 +1,26 @@
+<template>
+<div>
+<!--  <p> {{ products }}</p>-->
+</div>
+</template>
+
+<script>
+// import axios from "axios";
+
+export default {
+  // SSR ERROR 발생!! asyncData()는 pages 폴더에서만 사용이 가능하다
+  // async asyncData(){
+  //   const response = await axios.get('http://localhost:3000/products');
+  //   console.log(response);
+  //   this.products = response.data;
+    // const products = response.data;
+    // return { products }
+
+  // },
+  // name: "ProductList"
+}
+</script>
+
+<style scoped>
+
+</style>
--- pages/main.vue
+++ pages/main.vue
@@ -2,18 +2,54 @@
   <div>
     <h1>메인 페이지</h1>
     <p>메인 페이지입니다.</p>
+<!--    <ProductList></ProductList>-->
+<!--    {{ products }}-->
+    <ul>
+      <li v-for="product in products" :key="product.id">
+        <img :src="product.imageUrl" :alt="product.name">
+        <p>{{ product.name}}</p>
+        <p>{{ product.price}}</p>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
 import axios from 'axios';
-
+// import ProductList from "~/components/ProductList";
 export default {
-  name: "mainPage",
-  async created() {
-    const response = await axios.get('https://localhost:3000/products');
-    console.log(response);
 
+  async asyncData(){
+    const response = await axios.get('http://localhost:3000/products');
+    // this.products = response.data;
+    // asyncData() 에서는 this에 접근이 불가능 하다
+    const products = response.data.map((item) => ({
+        ...item,
+      // http://placeimg.com
+        imageUrl : `${item.imageUrl}?random=${Math.random()}`
+    }));
+    console.log(products);
+
+    return { products }
+
+  },
+
+  name: "mainPage",
+  data(){
+    return{
+      // products:[]
+    }
+  },
+
+  async created() {
+    // 기존 클라이언트 사이드 렌더링 방식의 데이터 호출
+    // const response = await axios.get('http://localhost:3000/products');
+    // console.log(response);
+    // this.products = response.data;
+
+  },
+  components:{
+    // ProductList
   }
 }
 </script>
