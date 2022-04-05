965645cc125dc234d68db9bc8680c003d7897d50
mwchoen <mwcheon@cudo.co.kr>
2022-04-05 09:12:59 +0900

    nuxt 다이나믹라우팅

---
+++ pages/detail/_id.vue
@@ -0,0 +1,19 @@
+<template>
+<div>
+  <h1>상세페이지</h1>
+  {{ $router }}
+</div>
+</template>
+
+<script>
+export default {
+  name: "_id",
+  created() {
+    console.log(this.$route)
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,99 @@
 <template>
-  <div>
-    <h1>홈 페이지</h1>
-    <p>넉스트 시작하기</p>
+  <div class="app">
+    <main>
+    <div>
+      <input type="text" name="" id="">
+    </div>
+
+      <ul>
+        <li  class="item flex" v-for="product in products" :key="product.id" @click="moveToDetailPage(product.id)">
+          <img class="product-image" :src="product.imageUrl" :alt="product.name">
+          <p>{{ product.name}}</p>
+          <span>{{ product.price}}</span>
+        </li>
+      </ul>
+    </main>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+// import ProductList from "~/components/ProductList";
 export default {
-  name: 'IndexPage',
+
+  async asyncData(){
+    const response = await axios.get('http://localhost:3000/products');
+    // this.products = response.data;
+    // asyncData() 에서는 this에 접근이 불가능 하다
+    const products = response.data.map((item) => ({
+      ...item,
+      // http://placeimg.com
+      imageUrl : `${item.imageUrl}?random=${Math.random()}`
+    }));
+    console.log(products);
+
+    return { products }
+
+  },
+
+  name: "mainPage",
+  data(){
+    return{
+      // products:[]
+    }
+  },
+
+  async created() {
+    // 기존 클라이언트 사이드 렌더링 방식의 데이터 호출
+    // const response = await axios.get('http://localhost:3000/products');
+    // console.log(response);
+    // this.products = response.data;
+
+  },
+  components:{
+    // ProductList
+  },
+  methods:{
+    moveToDetailPage(id){
+      console.log(id);
+      // nuxt에 라우터를 가지고 있어서 아래와 가이 쓸수 있다.
+      this.$router.push(`detail/${id}`);
+    }
+
+  }
 }
 </script>
+
+<style scoped>
+.flex {
+  display: flex;
+  justify-content: center;
+}
+.item {
+  display: inline-block;
+  width: 400px;
+  height: 300px;
+  text-align: center;
+  margin: 0 0.5rem;
+  cursor: pointer;
+}
+.product-image {
+  width: 400px;
+  height: 250px;
+}
+.app {
+  position: relative;
+}
+.cart-wrapper {
+  position: sticky;
+  float: right;
+  bottom: 50px;
+  right: 50px;
+}
+.cart-wrapper .btn {
+  display: inline-block;
+  height: 40px;
+  font-size: 1rem;
+  font-weight: 500;
+}
+</style>
