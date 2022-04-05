fc5384a0193bb0002826b1f00af6ab5ae6f962e6
mwchoen <mwcheon@cudo.co.kr>
2022-04-06 08:57:50 +0900

    css 스타일링 전역 설정

--- pages/detail/_id.vue
+++ pages/detail/_id.vue
@@ -1,13 +1,20 @@
 <template>
-<div>
-  <h1>상세페이지</h1>
   <div>
-    <img :src="product.imageUrl" :alt="product.name">
-    <p>name : {{ product.name}}</p>
-    <span>price : {{ product.price}}</span>
+    <div class="container">
+      <div class="main-panel">
+        <img
+          class="product-image"
+          :src="product.imageUrl"
+          :alt="product.name"
+        />
+      </div>
+      <div class="side-panel">
+        <p class="name">{{ product.name }}</p>
+        <p class="price">{{ product.price }}</p>
+<!--        <button type="button" @click="addToCart">Add to Cart</button>-->
+      </div>
+    </div>
   </div>
-
-</div>
 </template>
 
 <script>
@@ -34,5 +41,22 @@ export default {
 </script>
 
 <style scoped>
+.container {
+  display: flex;
+  justify-content: center;
+  margin: 2rem 0;
+}
+.product-image {
+  width: 500px;
+  height: 375px;
+}
+.side-panel {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  width: 220px;
+  text-align: center;
+  padding: 0 1rem;
+}
 
 </style>
