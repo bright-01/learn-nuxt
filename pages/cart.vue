6adb46a058901a8ee55664a90e23cc73ea9ce5b2
mwchoen <mwcheon@cudo.co.kr>
2022-04-12 20:14:17 +0900

    fetch 속성 적용

--- pages/cart.vue
+++ pages/cart.vue
@@ -2,17 +2,7 @@
 <template>
   <div class="container">
     <h1 class="list-title">담긴 상품 목록</h1>
-    <div class="list-wrapper">
-      <ul>
-        <li v-for="cart in $store.state.cartItems" :key="cart.id" class="list-item">
-          <img class="thumbnail" :src="cart.imageUrl" :alt="cart.name" />
-          <div class="description">
-            <p>{{ cart.name }}</p>
-            <span>{{ cart.price }}</span>
-          </div>
-        </li>
-      </ul>
-    </div>
+    <CartList></CartList>
     <div class="extra-panel">
       <button>구매하기</button>
     </div>
@@ -23,10 +13,13 @@
 
 
 import {FETCH_CART_ITEMS} from "~/store";
-
+import CartList from "~/components/CartList";
 export default {
   // eslint-disable-next-line vue/multi-word-component-names
   name: "cart",
+  components:{
+    CartList
+  }
   // async asyncData({ store }){
     // await store.dispatch(FETCH_CART_ITEMS);
   // }
